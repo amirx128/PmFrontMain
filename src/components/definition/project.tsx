e2b39285510c7b95f85cb25243f487e461d75bd7
Abolfazl Vaysi <vaysi.erfan@gmail.com>
2023-08-12 14:00:28 +0330

    add all projects view

---
+++ src/components/definition/project.tsx
@@ -0,0 +1,64 @@
+import {Box, Button, Grid, Typography} from "@mui/material";
+import moment from "jalali-moment";
+import {Apartment,Numbers} from "@mui/icons-material";
+
+export const ProjectCard = ({project}) => {
+    return (
+        <Grid item md={4} sm={6} xs={12}>
+            <Box sx={{
+                width: "100%",
+                height: "100%",
+                border: "2px solid #607D8B",
+                borderRadius: 2,
+                p:1
+            }}>
+                <Box display={"flex"} justifyContent={"space-between"} alignItems={"center"}>
+                    <Typography variant={"subtitle1"}>{project?.name}</Typography>
+                    <Typography variant={"subtitle1"}>{moment(project?.createDate).locale('fa').format('LL')}</Typography>
+                </Box>
+                <Box display={"flex"} gap={0.5} alignItems={"center"} mt={1}>
+                    <Apartment color={"secondary"} sx={{fontSize: 20}} />
+                    <Typography color={"secondary"} variant={"subtitle2"}>طبقات</Typography>
+                </Box>
+                <Box my={1}>
+                    {
+                        project?.projectfloor?.length > 0 ? (
+                            <Box display={"flex"} alignItems={"center"} gap={1} sx={{flexWrap:"wrap"}}>
+                                {project?.projectfloor?.map(floor => (
+                                    <Button size={"small"} color={"warning"} variant={"outlined"} sx={{display:"flex",flexDirection:"column",justifyContent:"center",flexGrow:1}}>
+                                        <Typography fontSize={16}>{floor?.name}</Typography>
+                                        <Typography fontSize={13}>کد : {floor?.code}</Typography>
+                                        <Typography fontSize={10}>{moment(floor?.createDate).locale('fa').format('LL')}</Typography>
+                                    </Button>
+                                ))}
+                            </Box>
+                            ) : (
+                            <Typography color={"error"} variant={"body2"}>طبقه ای تعریف نشده</Typography>
+                        )
+                    }
+                </Box>
+                <Box display={"flex"} gap={0.5} alignItems={"center"} mt={1}>
+                    <Numbers color={"secondary"} sx={{fontSize: 20}} />
+                    <Typography color={"secondary"} variant={"subtitle2"}>واحد ها</Typography>
+                </Box>
+                <Box my={1}>
+                    {
+                        project?.projectUnit?.length > 0 ? (
+                            <Box display={"flex"} alignItems={"center"} gap={1} sx={{flexWrap:"wrap"}}>
+                                {project?.projectUnit?.map(unit => (
+                                    <Button size={"small"} color={"info"} variant={"outlined"} sx={{display:"flex",flexDirection:"column",justifyContent:"center",flexGrow:1}}>
+                                        <Typography fontSize={16}>{unit?.name}</Typography>
+                                        <Typography fontSize={13}>کد : {unit?.code}</Typography>
+                                        <Typography fontSize={10}>{moment(unit?.createDate).locale('fa').format('LL')}</Typography>
+                                    </Button>
+                                ))}
+                            </Box>
+                        ) : (
+                            <Typography color={"error"} variant={"body2"}>واحدی تعریف نشده</Typography>
+                        )
+                    }
+                </Box>
+            </Box>
+        </Grid>
+    )
+}
--- src/pages/definitions/definitions.tsx
+++ src/pages/definitions/definitions.tsx
@@ -1,26 +1,41 @@
-import {Grid as CardGrid} from "@mui/material";
-import {useDispatch} from "react-redux";
+import {Box, Grid, Typography} from "@mui/material";
+import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import {getAllProjects} from "../../redux/features/definitionSlicer.ts";
-import {toast} from "react-toastify";
+import {Book} from "@mui/icons-material";
+import moment from 'jalali-moment'
+import {ProjectCard} from "../../components/definition/project.tsx";
 
 const Definitions = () => {
     const dispatch = useDispatch();
+    const {projects} = useSelector((state) => state.definition);
+
     useEffect(() => {
         dispatch(getAllProjects());
     }, [])
     return (
-        <CardGrid
-            item
-            xs={12}
-            sx={{
-                borderRadius: 2,
-                boxShadow: "0px 2px 10px 0px rgba(58, 53, 65, 0.1)",
-                marginBottom: "10px",
-            }}
+        <Grid
+            container
         >
-            sss
-        </CardGrid>
+            <Grid item xs={12}>
+                <Box sx={{
+                    borderRadius: 2,
+                    boxShadow: "0px 2px 10px 0px rgba(58, 53, 65, 0.1)",
+                    p: 2,
+                    width: "100%"
+                }}>
+                    <Typography sx={{display: "flex", alignItems: "center", mb: 1}}>
+                        <Book sx={{mr: 1}}/>
+                        پروژه ها
+                    </Typography>
+                    <Grid container spacing={1}>
+                        {
+                            projects?.data?.length > 0 ? projects?.data?.map(project => <ProjectCard project={project} key={project?.id} />) : 'no data'
+                        }
+                    </Grid>
+                </Box>
+            </Grid>
+        </Grid>
     );
 };
 export default Definitions;
