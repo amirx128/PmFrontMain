98c6598da46f2e27784d751666f972fd10503574
Abolfazl Vaysi <vaysi.erfan@gmail.com>
2023-08-21 18:04:08 +0330

    fix bugs

--- src/components/definition/addCommodity.tsx
+++ src/components/definition/addCommodity.tsx
@@ -155,117 +155,156 @@ export const AddCommodity = ({addCommodityDialog,onClose}) => {
     }
   }, [term]);
 
+  const [showTreeDialog,setShowTreeDialog] = useState(false);
+
   return (
-      <Dialog open={addCommodityDialog} onClose={onClose} fullWidth={true} maxWidth={'md'} fullScreen={mediumOrSmaller}>
-        <DialogTitle sx={{display:"flex",justifyContent:"space-between",alignItems:"center"}}>
-          افزودن کالا
-          <IconButton color={"error"} onClick={onClose}>
-            <HighlightOff />
-          </IconButton>
-        </DialogTitle>
-        <DialogContent>
-          <TextField value={info?.name} name={'name'} onChange={handleChange} label={'نام'} fullWidth={true} sx={{mt:2}}/>
-          <TextField value={info?.unit} name={'unit'} onChange={handleChange} label={'واحد'} fullWidth={true} sx={{mt:2}}/>
-          <TextField value={info?.description} name={'description'} onChange={handleChange} label={'توضیحات'} fullWidth={true} sx={{mt:2}}/>
-          <TextField value={info?.garanti} name={'garanti'} onChange={handleChange} label={'گارانتی'} fullWidth={true} sx={{mt:2}}/>
-          <FormControl fullWidth={true}>
-            <Typography sx={{mt: 2,mb:1}}>تامین کننده</Typography>
-            <Select value={info?.supplierId} labelId={"supplierId"} fullWidth={true} name={"supplierId"}
-                    onChange={handleChange}>
-              {suppliers?.data?.map(item => <MenuItem value={item.id} key={item?.id}>{item?.supplierName}</MenuItem>)}
-            </Select>
-          </FormControl>
-          <FormControl fullWidth={true}>
-            <Typography sx={{mt: 2,mb:1}}>تهیه کننده</Typography>
-            <Select value={info?.producerId} labelId={"producerId"} fullWidth={true} name={"producerId"}
-                    onChange={handleChange}>
-              {producers?.data?.map(item => <MenuItem value={item.id} key={item?.id}>{item?.name}</MenuItem>)}
-            </Select>
-          </FormControl>
-          <FormControl fullWidth={true}>
-            <Typography sx={{mt: 2,mb:1}}>زیر مجموعه ی کالای</Typography>
-            <Select value={info?.parentId} labelId={"parentId"} fullWidth={true} name={"parentId"}
-                    onChange={handleChange}>
-              {commodities?.data?.map(item => <MenuItem value={item.id} key={item?.id}>{item?.serchableName}</MenuItem>)}
-            </Select>
-          </FormControl>
-          <FormControl fullWidth={true}>
-            <Typography sx={{mt: 2,mb:1}}>نقش تجاری</Typography>
-            <Select multiple value={info?.businessRoleIds} labelId={"businessRoleIds"} fullWidth={true} name={"businessRoleIds"} label={"نقش تجاری"}
-                    onChange={handleChange}>
-              {businessRoles?.data?.map(item => <MenuItem value={item.id} key={item?.id}>{item?.name}</MenuItem>)}
-            </Select>
-          </FormControl>
-          <FormControl fullWidth={true}>
-            <Typography sx={{mt: 2,mb:1}}>پروژه مرتبط</Typography>
-            <Select multiple value={info?.useInProjectsIds} fullWidth={true} name={"useInProjectsIds"} label={"پروژه مرتبط"}
-                    onChange={handleChange}>
-              {getProjects().map(item => <MenuItem value={item.id} key={item?.id}>{item?.name}</MenuItem>)}
-            </Select>
-          </FormControl>
-          <FormControl fullWidth={true}>
-            <Box sx={{display:"flex",justifyContent:"space-between",alignItems:"center"}}>
-              <Typography sx={{mt: 2, mb: 1}}>انتخاب شاخه</Typography>
-              <TextField size={"small"} value={term} onChange={(e) => setTerm(e.target?.value)} InputProps={{
-                startAdornment: <InputAdornment position="start"><Search /></InputAdornment>,
-              }} placeholder={'جستجو در شاخه ها'} sx={{my:1}}/>
-            </Box>
-            <TreeView onNodeSelect={(e, ids) => handleChange({target: {value: ids, name: 'parentId'}})}
-                      defaultCollapseIcon={<ExpandMoreIcon/>}
-                      defaultExpandIcon={<ChevronLeftIcon/>}
-                      sx={{flexGrow: 1}}>
+      <>
+        <Dialog open={addCommodityDialog} onClose={onClose} fullWidth={true} maxWidth={'md'} fullScreen={mediumOrSmaller}>
+          <DialogTitle sx={{display:"flex",justifyContent:"space-between",alignItems:"center"}}>
+            افزودن کالا
+            <IconButton color={"error"} onClick={onClose}>
+              <HighlightOff />
+            </IconButton>
+          </DialogTitle>
+          <DialogContent>
+            <Grid spacing={1} container>
+              <Grid item xs={12} sm={6} md={4}>
+                <Typography sx={{mt: 2}}>نام</Typography>
+                <TextField value={info?.name} name={'name'} onChange={handleChange} fullWidth={true} sx={{mt:2}}/>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Typography sx={{mt: 2}}>واحد</Typography>
+                <TextField value={info?.unit} name={'unit'} onChange={handleChange} fullWidth={true} sx={{mt:2}}/>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Typography sx={{mt: 2}}>توضیحات</Typography>
+                <TextField value={info?.description} name={'description'} onChange={handleChange} fullWidth={true} sx={{mt:2}}/>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Typography sx={{mt: 2}}>گارانتی</Typography>
+                <TextField value={info?.garanti} name={'garanti'} onChange={handleChange} fullWidth={true} sx={{mt:2}}/>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <FormControl fullWidth={true}>
+                  <Typography sx={{mt: 2}}>تامین کننده</Typography>
+                  <Select sx={{mt:2}} value={info?.supplierId} labelId={"supplierId"} fullWidth={true} name={"supplierId"}
+                          onChange={handleChange}>
+                    {suppliers?.data?.map(item => <MenuItem value={item.id} key={item?.id}>{item?.supplierName}</MenuItem>)}
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <FormControl fullWidth={true}>
+                  <Typography sx={{mt: 2}}>تهیه کننده</Typography>
+                  <Select sx={{mt:2}} value={info?.producerId} labelId={"producerId"} fullWidth={true} name={"producerId"}
+                          onChange={handleChange}>
+                    {producers?.data?.map(item => <MenuItem value={item.id} key={item?.id}>{item?.name}</MenuItem>)}
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <FormControl fullWidth={true}>
+                  <Typography sx={{mt: 2}}>شاخه ی کالا</Typography>
+                  <Button sx={{mt:2}} fullWidth={true} color={"primary"} variant={"contained"} onClick={() => setShowTreeDialog(true)}>{info?.parentId ? (commoditiesOnTree?.data?.filter(item => item?.id === info?.parentId).length ? commoditiesOnTree?.data?.filter(item => item?.id === info?.parentId)[0].serchableName : 'انتخاب شاخه') : 'انتخاب شاخه'}</Button>
+                </FormControl>
+              </Grid>
+            </Grid>
+            <FormControl fullWidth={true}>
+              <Typography sx={{mt: 2,mb:1}}>نقش تجاری</Typography>
+              <Select multiple value={info?.businessRoleIds} labelId={"businessRoleIds"} fullWidth={true} name={"businessRoleIds"} label={"نقش تجاری"}
+                      onChange={handleChange}>
+                {businessRoles?.data?.map(item => <MenuItem value={item.id} key={item?.id}>{item?.name}</MenuItem>)}
+              </Select>
+            </FormControl>
+            <FormControl fullWidth={true}>
+              <Typography sx={{mt: 2,mb:1}}>پروژه مرتبط</Typography>
+              <Select multiple value={info?.useInProjectsIds} fullWidth={true} name={"useInProjectsIds"} label={"پروژه مرتبط"}
+                      onChange={handleChange}>
+                {getProjects().map(item => <MenuItem value={item.id} key={item?.id}>{item?.name}</MenuItem>)}
+              </Select>
+            </FormControl>
+            <FormControl fullWidth={true}>
+              <Typography sx={{mt: 2,mb:1}}>طبقه مرتبط</Typography>
+              <Select multiple value={info?.useInProjectsFloorIds} fullWidth={true} name={"useInProjectsFloorIds"} label={"طبقه مرتبط"}
+                      onChange={handleChange}>
+                {getFloors().map(item => <MenuItem value={item.id} key={item?.id}>{item?.name}</MenuItem>)}
+              </Select>
+            </FormControl>
+
+            <FormControl fullWidth={true}>
+              <Typography sx={{mt: 2,mb:1}}>واحد مرتبط</Typography>
+              <Select multiple value={info?.useInProjectsUnitsIds} fullWidth={true} name={"useInProjectsUnitsIds"} label={"واحد مرتبط"}
+                      onChange={handleChange}>
+                {getUnits().map(item => <MenuItem value={item.id} key={item?.id}>{item?.name}</MenuItem>)}
+              </Select>
+            </FormControl>
+            <Box sx={{width:"100%"}}>
+              <Typography sx={{mt: 2,mb:1}}>خصوصیات</Typography>
+              <Box sx={{display:"flex",flexWrap:"wrap",alignItems:"center",my:1}}>
+                {
+                  info?.props?.map(item => <Chip label={`${item?.propName} : ${item?.propValue}`} onDelete={() => handleDeleteProp(item)} />)
+                }
+              </Box>
               {
-                  commoditiesOnTree?.data?.length && getTreeDate()
+                  showAddProp && (
+                      <Grid container spacing={1}>
+                        <Grid item xs={12} md={4}>
+                          <TextField value={propVal?.propName} name={'propName'} onChange={handleChangeProp} placeholder={'نام خصوصیت'} fullWidth={true}/>
+                        </Grid>
+                        <Grid item xs={12} md={4}>
+                          <Select value={propVal?.propType} placeholder="نوع خصوصیت" fullWidth={true} name={"propType"}
+                                  onChange={handleChangeProp}>
+                            <MenuItem value={'integer'}>عدد</MenuItem>
+                            <MenuItem value={'string'}>رشته</MenuItem>
+                            <MenuItem value={'date'}>تاریخ</MenuItem>
+                          </Select>
+                        </Grid>
+                        <Grid item xs={12} md={4}>
+                          <TextField value={propVal?.propValue} name={'propValue'} onChange={handleChangeProp} placeholder={'مقدار خصوصیت'} fullWidth={true}/>
+                        </Grid>
+                        <Grid item xs={12}>
+                          <Button disabled={!propVal?.propName || !propVal?.propType || !propVal?.propValue } fullWidth={true} variant={"contained"} color={"primary"} onClick={handleAddProp}>تایید خصوصیت</Button>
+                        </Grid>
+                      </Grid>
+                  )
               }
-            </TreeView>
-          </FormControl>
-          <FormControl fullWidth={true}>
-            <Typography sx={{mt: 2,mb:1}}>طبقه مرتبط</Typography>
-            <Select multiple value={info?.useInProjectsFloorIds} fullWidth={true} name={"useInProjectsFloorIds"} label={"طبقه مرتبط"}
-                    onChange={handleChange}>
-              {getFloors().map(item => <MenuItem value={item.id} key={item?.id}>{item?.name}</MenuItem>)}
-            </Select>
-          </FormControl>
-          <Box sx={{width:"100%"}}>
-            <Typography sx={{mt: 2,mb:1}}>خصوصیات</Typography>
-            <Box sx={{display:"flex",flexWrap:"wrap",alignItems:"center",my:1}}>
-              {
-                info?.props?.map(item => <Chip label={`${item?.propName} : ${item?.propValue}`} onDelete={() => handleDeleteProp(item)} />)
-              }
+              <Button sx={{my:1}} fullWidth={true} variant={"outlined"} startIcon={<Add />} onClick={() => setShowAddProp(true)}>
+                افزودن خصوصیت
+              </Button>
             </Box>
-            {
-                showAddProp && (
-                    <Grid container spacing={1}>
-                      <Grid item xs={12} md={4}>
-                        <TextField value={propVal?.propName} name={'propName'} onChange={handleChangeProp} placeholder={'نام خصوصیت'} fullWidth={true}/>
-                      </Grid>
-                      <Grid item xs={12} md={4}>
-                        <Select value={propVal?.propType} placeholder="نوع خصوصیت" fullWidth={true} name={"propType"}
-                                onChange={handleChangeProp}>
-                          <MenuItem value={'integer'}>عدد</MenuItem>
-                          <MenuItem value={'string'}>رشته</MenuItem>
-                          <MenuItem value={'date'}>تاریخ</MenuItem>
-                        </Select>
-                      </Grid>
-                      <Grid item xs={12} md={4}>
-                        <TextField value={propVal?.propValue} name={'propValue'} onChange={handleChangeProp} placeholder={'مقدار خصوصیت'} fullWidth={true}/>
-                      </Grid>
-                      <Grid item xs={12}>
-                        <Button disabled={!propVal?.propName || !propVal?.propType || !propVal?.propValue } fullWidth={true} variant={"contained"} color={"primary"} onClick={handleAddProp}>تایید خصوصیت</Button>
-                      </Grid>
-                    </Grid>
-                )
-            }
-            <Button sx={{my:1}} fullWidth={true} variant={"outlined"} startIcon={<Add />} onClick={() => setShowAddProp(true)}>
-              افزودن خصوصیت
+          </DialogContent>
+          <DialogActions>
+            <Button variant={"contained"} color={"success"} onClick={onSubmit}>
+              {businessRoles?.addState ? <CircularProgress /> : 'ثبت'}
             </Button>
-          </Box>
-        </DialogContent>
-        <DialogActions>
-          <Button variant={"contained"} color={"success"} onClick={onSubmit}>
-            {businessRoles?.addState ? <CircularProgress /> : 'ثبت'}
-          </Button>
-        </DialogActions>
-      </Dialog>
+          </DialogActions>
+        </Dialog>
+        <Dialog open={showTreeDialog} onClose={() => setShowTreeDialog(false)} fullWidth={true} maxWidth={'md'} fullScreen={mediumOrSmaller}>
+          <DialogTitle sx={{display:"flex",justifyContent:"space-between",alignItems:"center"}}>
+            انتخاب شاخه کالا
+            <IconButton color={"error"} onClick={() => setShowTreeDialog(false)}>
+              <HighlightOff />
+            </IconButton>
+          </DialogTitle>
+          <DialogContent>
+            <TextField size={"small"} fullWidth={true} value={term} onChange={(e) => setTerm(e.target?.value)} InputProps={{
+              startAdornment: <InputAdornment position="start"><Search /></InputAdornment>,
+            }} placeholder={'جستجو در شاخه ها'} sx={{my:1}}/>
+          <TreeView onNodeSelect={(e, ids) => handleChange({target: {value: ids, name: 'parentId'}})}
+                    defaultCollapseIcon={<ExpandMoreIcon/>}
+                    defaultExpandIcon={<ChevronLeftIcon/>}
+                    sx={{flexGrow: 1}}>
+            {
+                commoditiesOnTree?.data?.length && getTreeDate()
+            }
+          </TreeView>
+          </DialogContent>
+          <DialogActions>
+            <Button color={"secondary"} variant={"contained"} fullWidth={true} onClick={() => setShowTreeDialog(false)}>
+              تایید
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </>
   );
 }
