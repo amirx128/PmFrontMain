a907985bec7916bf0df6eb07daa35d19161b2064
developerfront247 <developerfront247@gmail.com>
2023-10-19 23:39:50 +0330

    finish sub Item

--- src/pages/QC/addSubItem.tsx
+++ src/pages/QC/addSubItem.tsx
@@ -5,38 +5,77 @@ import {
   CardContent,
   CardHeader,
   FormControl,
+  IconButton,
   InputLabel,
   MenuItem,
   Select,
   TextField,
 } from "@mui/material";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   AddNewSubItemAction,
+  GetAllCheckListsAction,
+  GetAllContractorAction,
   GetAllOriginalItemsAction,
+  GetAllUsabilityAction,
 } from "../../redux/features/qcSlicer";
 import { LoadingButton } from "@mui/lab";
 import { useNavigate } from "react-router-dom";
+import { GetAllProjects_Floor_Unit_UsabilityAction } from "../../redux/features/definitionSlicer";
+import CloseIcon from "@mui/icons-material/Close";
 const AddSubItem = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch<any>();
-  const { AddNewSubItem, originalItems } = useSelector(
-    (state: any) => state?.qc
+  const { AddNewSubItem, originalItems, usabilities, checkLists, contractors } =
+    useSelector((state: any) => state?.qc);
+  const { allProjectsFloorUnitUsability } = useSelector(
+    (state: any) => state?.definition
   );
 
   const [info, setInfo] = useState({
     name: "",
     originalItemId: undefined,
+    usabilities: [],
+    masterCheckListId: undefined,
+    projectId: undefined,
+    floorId: undefined,
+    unitId: undefined,
+    usabilityId: undefined,
+    contractorId: undefined,
   });
+  const [showAddNewWorkData, setShowAddNewWorkData] = useState<boolean>(false);
+  const [workingData, setWorkingData] = useState([]);
+  const getAllOriginalItems = useCallback(async () => {
+    await dispatch(GetAllOriginalItemsAction());
+  }, [dispatch]);
+  const getAllUsabilities = useCallback(async () => {
+    await dispatch(GetAllUsabilityAction());
+  }, [dispatch]);
+  const getAllChecklists = useCallback(async () => {
+    await dispatch(GetAllCheckListsAction());
+  }, [dispatch]);
+  const getAllProjects = useCallback(async () => {
+    await dispatch(GetAllProjects_Floor_Unit_UsabilityAction());
+  }, [dispatch]);
+  const getAllContractor = useCallback(async () => {
+    await dispatch(GetAllContractorAction());
+  }, [dispatch]);
 
   useEffect(() => {
     getAllOriginalItems();
-  }, []);
+    getAllUsabilities();
+    getAllChecklists();
+    getAllProjects();
+    getAllContractor();
+  }, [
+    getAllOriginalItems,
+    getAllUsabilities,
+    getAllChecklists,
+    getAllProjects,
+    getAllContractor,
+  ]);
 
-  const getAllOriginalItems = async () => {
-    await dispatch(GetAllOriginalItemsAction());
-  };
   const handleChange = (e) => {
     setInfo({
       ...info,
@@ -47,9 +86,45 @@ const AddSubItem = () => {
     await dispatch(
       AddNewSubItemAction({
         name: info.name,
-        originalItemId: info.originalItemId,
+        originalItemId: +info.originalItemId,
+        usabilities: info.usabilities,
+        masterCheckListId: +info.masterCheckListId,
+        workingData: workingData
+          .filter(
+            (work) => work.contractorId && work.usabilityId && work.unitId
+          )
+          .map((work) => ({
+            contractorId: work.contractorId,
+            usabilityId: +work.usabilityId,
+            unitId: +work.unitId,
+          })),
       })
     );
+  };
+  const handleDeleteItem = (id) => {
+    setWorkingData((prev) => prev.filter((p) => +p.id !== +id));
+  };
+  const handleAddItem = () => {
+    setWorkingData((prev) => [
+      ...prev,
+      {
+        id: prev.length,
+        projectId: info.projectId,
+        floorId: info.floorId,
+        contractorId: info?.contractorId,
+        usabilityId: info?.usabilityId,
+        unitId: info?.unitId,
+      },
+    ]);
+    setInfo((prev) => ({
+      ...prev,
+      projectId: undefined,
+      floorId: undefined,
+      unitId: undefined,
+      usabilityId: undefined,
+      contractorId: undefined,
+    }));
+    setShowAddNewWorkData(false);
   };
   return (
     <Card>
@@ -68,22 +143,205 @@ const AddSubItem = () => {
           label={"نام"}
           sx={{ mt: 2, width: "50%" }}
         />
+        <div className="flex w-full gap-20">
+          <FormControl sx={{ mt: 2, width: "50%" }}>
+            <InputLabel>آیتم اصلی</InputLabel>
+            <Select
+              value={info?.originalItemId}
+              fullWidth={true}
+              name={"originalItemId"}
+              label="آیتم اصلی"
+              onChange={handleChange}
+            >
+              {originalItems?.data?.map((item) => (
+                <MenuItem value={item.id} key={item?.id}>
+                  {item?.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl sx={{ mt: 2, width: "50%" }}>
+            <InputLabel>چک لیست اصلی</InputLabel>
+            <Select
+              value={info?.masterCheckListId}
+              fullWidth={true}
+              name={"masterCheckListId"}
+              label="آیتم اصلی"
+              onChange={handleChange}
+            >
+              {checkLists?.data?.map((item) => (
+                <MenuItem value={item.id} key={item?.id}>
+                  {item?.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
         <FormControl sx={{ mt: 2, width: "50%" }}>
-          <InputLabel>آیتم اصلی</InputLabel>
+          <InputLabel>کاربری</InputLabel>
           <Select
-            value={info?.originalItemId}
+            value={info?.usabilities}
             fullWidth={true}
-            name={"originalItemId"}
-            label="آیتم اصلی"
+            name={"usabilities"}
+            label="کاربری"
             onChange={handleChange}
+            multiple
           >
-            {originalItems?.data?.map((item) => (
+            {usabilities?.data?.map((item) => (
               <MenuItem value={item.id} key={item?.id}>
-                {item?.name}
+                {item?.usablityName}
               </MenuItem>
             ))}
           </Select>
         </FormControl>
+        {!!workingData.length && (
+          <div className="mt-6 flex gap-6">
+            {workingData.map((work, index) => (
+              <div
+                key={index}
+                className="bg-slate-200 px-6 py-3 rounded-2xl text-xs"
+              >
+                {
+                  allProjectsFloorUnitUsability.data
+                    ?.find((project) => +project.id === +work.projectId)
+                    ?.projectfloor?.find((floor) => +floor.id === +work.floorId)
+                    ?.projectUnit?.find((unit) => +unit.id === +work.unitId)
+                    ?.name
+                }
+                /
+                {
+                  allProjectsFloorUnitUsability.data
+                    ?.find((project) => +project.id === +work.projectId)
+                    ?.projectfloor?.find((floor) => +floor.id === +work.floorId)
+                    ?.projectUnit?.find((unit) => +unit.id === +work.unitId)
+                    ?.unitsUsability?.find(
+                      (usa) => +usa.id === +work.usabilityId
+                    )?.name
+                }
+                /
+                {
+                  contractors.data?.find((cont) => cont.id == work.contractorId)
+                    ?.fullName
+                }
+                <IconButton onClick={() => handleDeleteItem(work.id)}>
+                  <CloseIcon color="error" />
+                </IconButton>
+              </div>
+            ))}
+          </div>
+        )}
+        <div className="mt-10 w-full flex flex-col items-start gap-8">
+          <Button
+            variant="outlined"
+            color="secondary"
+            disabled={showAddNewWorkData}
+            onClick={() => setShowAddNewWorkData(true)}
+          >
+            دیتای کاری جدید +
+          </Button>
+
+          {showAddNewWorkData && (
+            <div className="w-2/3 grid grid-cols-2 gap-12">
+              <FormControl className="w-full">
+                <InputLabel>پروژه</InputLabel>
+                <Select
+                  value={info?.projectId}
+                  fullWidth={true}
+                  name={"projectId"}
+                  label="آیتم اصلی"
+                  onChange={handleChange}
+                >
+                  {allProjectsFloorUnitUsability?.data?.map((item) => (
+                    <MenuItem value={item.id} key={item?.id}>
+                      {item?.name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <FormControl className="w-full">
+                <InputLabel>طبقه</InputLabel>
+                <Select
+                  value={info?.floorId}
+                  fullWidth={true}
+                  name={"floorId"}
+                  label="آیتم اصلی"
+                  onChange={handleChange}
+                >
+                  {allProjectsFloorUnitUsability?.data
+                    ?.find((project) => +project.id === info.projectId)
+                    ?.projectfloor?.map((item) => (
+                      <MenuItem value={item.id} key={item?.id}>
+                        {item?.name}
+                      </MenuItem>
+                    ))}
+                </Select>
+              </FormControl>
+              <FormControl className="w-full">
+                <InputLabel>واحد</InputLabel>
+                <Select
+                  value={info?.unitId}
+                  fullWidth={true}
+                  name={"unitId"}
+                  label="آیتم اصلی"
+                  onChange={handleChange}
+                >
+                  {allProjectsFloorUnitUsability?.data
+                    ?.find((project) => +project.id === info.projectId)
+                    ?.projectfloor?.find((floor) => floor.id === info.floorId)
+                    ?.projectUnit?.map((item) => (
+                      <MenuItem value={item.id} key={item?.id}>
+                        {item?.name}
+                      </MenuItem>
+                    ))}
+                </Select>
+              </FormControl>
+              <FormControl className="w-full">
+                <InputLabel>کاربری</InputLabel>
+                <Select
+                  value={info?.usabilityId}
+                  fullWidth={true}
+                  name={"usabilityId"}
+                  label="آیتم اصلی"
+                  onChange={handleChange}
+                >
+                  {allProjectsFloorUnitUsability?.data
+                    ?.find((project) => +project.id === info.projectId)
+                    ?.projectfloor?.find((floor) => floor.id === info.floorId)
+                    ?.projectUnit?.find((unit) => +unit.id === +info.unitId)
+                    ?.unitsUsability?.map((item) => (
+                      <MenuItem value={item.id} key={item?.id}>
+                        {item?.name}
+                      </MenuItem>
+                    ))}
+                </Select>
+              </FormControl>
+              <FormControl className="w-full">
+                <InputLabel>پیمانکار</InputLabel>
+                <Select
+                  value={info?.contractorId}
+                  fullWidth={true}
+                  name={"contractorId"}
+                  label="آیتم اصلی"
+                  onChange={handleChange}
+                >
+                  {contractors?.data?.map((item) => (
+                    <MenuItem value={item.id} key={item?.id}>
+                      {item?.fullName}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+
+              <Button
+                variant="contained"
+                className="col-span-full"
+                onClick={handleAddItem}
+              >
+                اضافه کردن
+              </Button>
+            </div>
+          )}
+        </div>
       </CardContent>
       <CardActions sx={{ display: "flex", justifyContent: "flex-end" }}>
         <LoadingButton
--- src/pages/QC/editSubItem.tsx
+++ src/pages/QC/editSubItem.tsx
@@ -6,19 +6,23 @@ import {
   CardHeader,
   FormControl,
   FormControlLabel,
+  IconButton,
   InputLabel,
   MenuItem,
   Select,
   Switch,
   TextField,
 } from "@mui/material";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   AddNewOriginalItemAction,
   AddNewSubItemAction,
+  GetAllCheckListsAction,
+  GetAllContractorAction,
   GetAllOriginalItemsAction,
   GetAllSubItemsAction,
+  GetAllUsabilityAction,
   GetSubItemsDataAction,
   UpdateSubItemAction,
 } from "../../redux/features/qcSlicer";
@@ -26,26 +30,73 @@ import { LoadingButton } from "@mui/lab";
 import { useNavigate, useParams } from "react-router-dom";
 import JalaliDatePicker from "../../components/date-picker/date-picker";
 import { GetUsersListAction } from "../../redux/features/administrationSlicer";
+import { GetAllProjects_Floor_Unit_UsabilityAction } from "../../redux/features/definitionSlicer";
+import CloseIcon from "@mui/icons-material/Close";
+
 const EditSubItem = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const dispatch = useDispatch<any>();
-  const { subItemsUpdateState, originalItems, selectedSubItem } = useSelector(
-    (state: any) => state?.qc
-  );
-  const { usersList } = useSelector(
-    (state: any) => state?.administrations?.users
+  const {
+    subItemsUpdateState,
+    originalItems,
+    selectedSubItem,
+    usabilities,
+    checkLists,
+    contractors,
+  } = useSelector((state: any) => state?.qc);
+  const { allProjectsFloorUnitUsability } = useSelector(
+    (state: any) => state?.definition
   );
 
   const [info, setInfo] = useState({
     name: "",
     originalItemId: 0,
+    usabilities: [],
+    masterCheckListId: undefined,
+    projectId: undefined,
+    floorId: undefined,
+    unitId: undefined,
+    usabilityId: undefined,
+    contractorId: undefined,
   });
+  const [showAddNewWorkData, setShowAddNewWorkData] = useState<boolean>(false);
+  const [workingData, setWorkingData] = useState([]);
+
+  const getSubItem = useCallback(async () => {
+    await dispatch(GetSubItemsDataAction({ selectedItemId: +id }));
+  }, [dispatch]);
+  const getAllOriginalItems = useCallback(async () => {
+    await dispatch(GetAllOriginalItemsAction());
+  }, [dispatch]);
+  const getAllUsabilities = useCallback(async () => {
+    await dispatch(GetAllUsabilityAction());
+  }, [dispatch]);
+  const getAllChecklists = useCallback(async () => {
+    await dispatch(GetAllCheckListsAction());
+  }, [dispatch]);
+  const getAllProjects = useCallback(async () => {
+    await dispatch(GetAllProjects_Floor_Unit_UsabilityAction());
+  }, [dispatch]);
+  const getAllContractor = useCallback(async () => {
+    await dispatch(GetAllContractorAction());
+  }, [dispatch]);
 
   useEffect(() => {
     getSubItem();
     getAllOriginalItems();
-  }, []);
+    getAllUsabilities();
+    getAllChecklists();
+    getAllProjects();
+    getAllContractor();
+  }, [
+    getSubItem,
+    getAllOriginalItems,
+    getAllUsabilities,
+    getAllChecklists,
+    getAllProjects,
+    getAllContractor,
+  ]);
 
   useEffect(() => {
     if (selectedSubItem?.data) {
@@ -53,15 +104,13 @@ const EditSubItem = () => {
         ...info,
         name: selectedSubItem?.data.name,
         originalItemId: selectedSubItem?.data.originalItemId,
+        masterCheckListId: selectedSubItem?.data.masterCheckListId,
+        usabilities: selectedSubItem?.data.usabilities || [],
       });
+      setWorkingData(selectedSubItem?.data.workingDatas);
     }
   }, [selectedSubItem]);
-  const getSubItem = async () => {
-    await dispatch(GetSubItemsDataAction({ selectedItemId: +id }));
-  };
-  const getAllOriginalItems = async () => {
-    await dispatch(GetAllOriginalItemsAction());
-  };
+
   const handleChange = (e) => {
     setInfo({
       ...info,
@@ -75,10 +124,46 @@ const EditSubItem = () => {
         data: {
           name: info.name,
           originalItemId: info.originalItemId,
+          usabilities: info.usabilities,
+          masterCheckListId: info.masterCheckListId,
+          workingData: workingData
+            .filter(
+              (work) => work.contractorId || work.usabilityId || work.unitId
+            )
+            .map((work) => ({
+              contractorId: work.contractorId,
+              usabilityId: work.usabilityId,
+              unitId: work.unitId,
+            })),
         },
       })
     );
     await getSubItem();
+  };
+  const handleDeleteItem = (id) => {
+    setWorkingData((prev) => prev.filter((p) => +p.id !== +id));
+  };
+  const handleAddItem = () => {
+    setWorkingData((prev) => [
+      ...prev,
+      {
+        id: prev.length,
+        projectId: info.projectId,
+        floorId: info.floorId,
+        contractorId: info?.contractorId,
+        usabilityId: info?.usabilityId,
+        unitId: info?.unitId,
+      },
+    ]);
+    setInfo((prev) => ({
+      ...prev,
+      projectId: undefined,
+      floorId: undefined,
+      unitId: undefined,
+      usabilityId: undefined,
+      contractorId: undefined,
+    }));
+    setShowAddNewWorkData(false);
   };
   return (
     <Card>
@@ -113,6 +198,187 @@ const EditSubItem = () => {
             ))}
           </Select>
         </FormControl>
+        <FormControl sx={{ mt: 2, width: "50%" }}>
+          <InputLabel>چک لیست اصلی</InputLabel>
+          <Select
+            value={info?.masterCheckListId}
+            fullWidth={true}
+            name={"masterCheckListId"}
+            label="آیتم اصلی"
+            onChange={handleChange}
+          >
+            {selectedSubItem?.data?.relatedCheckList?.map((item) => (
+              <MenuItem value={item.id} key={item?.id}>
+                {item?.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl sx={{ mt: 2, width: "50%" }}>
+          <InputLabel>کاربری</InputLabel>
+          <Select
+            value={info?.usabilities}
+            fullWidth={true}
+            name={"usabilities"}
+            label="کاربری"
+            onChange={handleChange}
+            multiple
+          >
+            {usabilities?.data?.map((item) => (
+              <MenuItem value={item.id} key={item?.id}>
+                {item?.usablityName}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        {!!workingData.length && (
+          <div className="mt-6 flex gap-6">
+            {workingData.map((work, index) => (
+              <div
+                key={index}
+                className="bg-slate-200 px-6 py-3 rounded-2xl text-xs"
+              >
+                {
+                  allProjectsFloorUnitUsability.data
+                    ?.find((project) => +project.id === +work.projectId)
+                    ?.projectfloor?.find((floor) => +floor.id === +work.floorId)
+                    ?.projectUnit?.find((unit) => +unit.id === +work.unitId)
+                    ?.name
+                }
+                /
+                {
+                  allProjectsFloorUnitUsability.data
+                    ?.find((project) => +project.id === +work.projectId)
+                    ?.projectfloor?.find((floor) => +floor.id === +work.floorId)
+                    ?.projectUnit?.find((unit) => +unit.id === +work.unitId)
+                    ?.unitsUsability?.find(
+                      (usa) => +usa.id === +work.usabilityId
+                    )?.name
+                }
+                /
+                {
+                  contractors.data?.find((cont) => cont.id == work.contractorId)
+                    ?.fullName
+                }
+                <IconButton onClick={() => handleDeleteItem(work.id)}>
+                  <CloseIcon color="error" />
+                </IconButton>
+              </div>
+            ))}
+          </div>
+        )}
+        <div className="mt-10 w-full flex flex-col items-start gap-8">
+          <Button
+            variant="outlined"
+            color="secondary"
+            disabled={showAddNewWorkData}
+            onClick={() => setShowAddNewWorkData(true)}
+          >
+            دیتای کاری جدید +
+          </Button>
+
+          {showAddNewWorkData && (
+            <div className="w-2/3 grid grid-cols-2 gap-12">
+              <FormControl className="w-full">
+                <InputLabel>پروژه</InputLabel>
+                <Select
+                  value={info?.projectId}
+                  fullWidth={true}
+                  name={"projectId"}
+                  label="آیتم اصلی"
+                  onChange={handleChange}
+                >
+                  {allProjectsFloorUnitUsability?.data?.map((item) => (
+                    <MenuItem value={item.id} key={item?.id}>
+                      {item?.name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <FormControl className="w-full">
+                <InputLabel>طبقه</InputLabel>
+                <Select
+                  value={info?.floorId}
+                  fullWidth={true}
+                  name={"floorId"}
+                  label="آیتم اصلی"
+                  onChange={handleChange}
+                >
+                  {allProjectsFloorUnitUsability?.data
+                    ?.find((project) => +project.id === info.projectId)
+                    ?.projectfloor?.map((item) => (
+                      <MenuItem value={item.id} key={item?.id}>
+                        {item?.name}
+                      </MenuItem>
+                    ))}
+                </Select>
+              </FormControl>
+              <FormControl className="w-full">
+                <InputLabel>واحد</InputLabel>
+                <Select
+                  value={info?.unitId}
+                  fullWidth={true}
+                  name={"unitId"}
+                  label="آیتم اصلی"
+                  onChange={handleChange}
+                >
+                  {allProjectsFloorUnitUsability?.data
+                    ?.find((project) => +project.id === info.projectId)
+                    ?.projectfloor?.find((floor) => floor.id === info.floorId)
+                    ?.projectUnit?.map((item) => (
+                      <MenuItem value={item.id} key={item?.id}>
+                        {item?.name}
+                      </MenuItem>
+                    ))}
+                </Select>
+              </FormControl>
+              <FormControl className="w-full">
+                <InputLabel>کاربری</InputLabel>
+                <Select
+                  value={info?.usabilityId}
+                  fullWidth={true}
+                  name={"usabilityId"}
+                  label="آیتم اصلی"
+                  onChange={handleChange}
+                >
+                  {allProjectsFloorUnitUsability?.data
+                    ?.find((project) => +project.id === info.projectId)
+                    ?.projectfloor?.find((floor) => floor.id === info.floorId)
+                    ?.projectUnit?.find((unit) => +unit.id === +info.unitId)
+                    ?.unitsUsability?.map((item) => (
+                      <MenuItem value={item.id} key={item?.id}>
+                        {item?.name}
+                      </MenuItem>
+                    ))}
+                </Select>
+              </FormControl>
+              <FormControl className="w-full">
+                <InputLabel>پیمانکار</InputLabel>
+                <Select
+                  value={info?.contractorId}
+                  fullWidth={true}
+                  name={"contractorId"}
+                  label="آیتم اصلی"
+                  onChange={handleChange}
+                >
+                  {contractors?.data?.map((item) => (
+                    <MenuItem value={item.id} key={item?.id}>
+                      {item?.fullName}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+
+              <Button
+                variant="contained"
+                className="col-span-full"
+                onClick={handleAddItem}
+              >
+                اضافه کردن
+              </Button>
+            </div>
+          )}
+        </div>
       </CardContent>
 
       <CardActions sx={{ display: "flex", justifyContent: "flex-end" }}>
