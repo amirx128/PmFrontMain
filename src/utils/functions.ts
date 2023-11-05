f200886b00ad3407689c57157daadbfa90871799
developerfront247 <developerfront247@gmail.com>
2023-11-05 23:29:56 +0330

    add number format

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,3 +1,8 @@
 export const getUserIdFromStorage = () => {
-    return localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user'))?.id : '1';
-}
+  return localStorage.getItem('user')
+    ? JSON.parse(localStorage.getItem('user'))?.id
+    : '1';
+};
+export const formatNumber = (value: any) => {
+  return new Intl.NumberFormat().format(+value);
+};
